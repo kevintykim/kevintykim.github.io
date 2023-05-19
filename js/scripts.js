67d418163e91a05ee68a04423116b66564276680
kevintykim <114948536+kevintykim@users.noreply.github.com>
2023-05-18 23:44:25 -0700

    General updates

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,13 @@ function menuToggle() {
         x.className = "navtoggle";
     }
 }
+
+$(document).ready(function() {
+    var d = new Date();
+	var year = d.getFullYear();
+	if(year === 2022) {
+		$(".copyright").html("<h3>&#169; " + year + " &nbsp;KEVIN KIM</h3>");
+	} else {
+		$(".copyright").html("<h3>&#169; 2022 &#8211; " + year + " &nbsp;KEVIN KIM</h3>");
+	};
+})
